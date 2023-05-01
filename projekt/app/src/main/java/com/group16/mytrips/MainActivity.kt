d38351f1997723459fbdea1fcf68b219021dd3d5
Ale299 <ale29@live.de>
2023-05-01 15:51:53 +0200

    Kamera Permission added

--- projekt/app/src/main/java/com/group16/mytrips/MainActivity.kt
+++ projekt/app/src/main/java/com/group16/mytrips/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.group16.mytrips
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,25 +22,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.group16.mytrips.screens.PreviewHeader
 
 import com.group16.mytrips.ui.theme.MyTripsTheme
 
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyTripsTheme {
+
+    class MainActivity : ComponentActivity() {
+        private val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) Log.i("ja", "Wurde erlaubt")
+            else Log.i("ja", "Nicht erlaubt")
+        }
+
+        @OptIn(ExperimentalMaterial3Api::class)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                MyTripsTheme {
 
 
+                }
+            }
+        }
+
+        private fun requestCameraPermission() {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CAMERA
+                )
+                        == PackageManager.PERMISSION_GRANTED -> {
+                    Log.i("ja", "Kamera zugriff bereits erlaubt")
+                }
+
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.CAMERA
+                )
+                -> Log.i("ja", "Zeige Kamera Permission Text")
+
+                else -> requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
             }
         }
     }
 }
-
-
 
 
 @Composable
